6a11ba5f45fe713dd168afd8838e33d97542ba24
huy <huy.nguyen@monimedia.com>
2019-07-29 14:44:02 +0700

    Add hikes route and template

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var hike = require('./routes/hike');
 
 var app = express();
-
+app.get('/hikes', hike.index);
+app.post('/add_hike', hike.add_hike);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
