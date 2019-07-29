8d38244d7bc34b4741d0a5fc440dd936c42e4528
huy <huy.nguyen@monimedia.com>
2019-07-29 15:20:25 +0700

    add cors

--- app.js
+++ app.js
@@ -23,6 +23,26 @@ mongoose.connect('mongodb://vhuy2571990:langtu1990!@ds133187.mlab.com:33187/hero
     process.exit();
   })
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.get('/hikes', hike.index);
 app.post('/add_hike', hike.add_hike);
 // view engine setup
@@ -33,6 +53,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 //app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
